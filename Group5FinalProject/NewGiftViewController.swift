2f4ee2b9508756f22d4cedc32623e47ab8599424
Brayden Peeples <braydenpeeples@gmail.com>
2026-04-16 04:01:08 -0400

    Other changes.

--- Group5FinalProject/NewGiftViewController.swift
+++ Group5FinalProject/NewGiftViewController.swift
@@ -5,10 +5,28 @@
 //  Created by Brayden Peeples on 4/7/26.
 //
 
-import Foundation
+import UIKit
 
-class NewGiftViewController {
+class NewGiftViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var giftTitle: UITextField!
+    @IBOutlet weak var giftDescription: UITextView!
+    @IBOutlet weak var giftHyperlink: UITextField!
+    @IBOutlet weak var giftImage: UIImageView!
+    @IBAction func selectImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
+        giftImage.image = image
+        
+        dismiss(animated: true)
+    }
     
 }
+
