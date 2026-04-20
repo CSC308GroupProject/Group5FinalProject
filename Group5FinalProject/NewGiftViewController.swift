c956decca6cb16a63818ddfd2be53d6e2d6c44bb
Brayden Peeples <braydenpeeples@gmail.com>
2026-04-20 00:41:59 -0400

    Added comments to signify purpose of code segments. Also added a comment in NewGiftViewController to show where the check for gift title should go.

--- Group5FinalProject/NewGiftViewController.swift
+++ Group5FinalProject/NewGiftViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class NewGiftViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    // References assigned by main viewController when table cell is clicked.
     var mainVC: ViewController?
     var giftIndex: Int?
     
@@ -16,22 +17,41 @@ class NewGiftViewController: UIViewController, UIImagePickerControllerDelegate, 
     var descriptionVar: String?
     var hyperlinkVar: String?
     var imageVar: UIImage?
+    
     var deleteHidden = true
     
+    
+    // Action performed when "done" button is clicked.
     @IBAction func doneButton(_ sender: Any) {
+        
+        /*
+         
+         CHECK HERE IF GIFTTITLE IS ASSIGNED A VALUE
+         CALL UIALERT IF NOT
+         
+         */
+        
+        // If VC was called using "add" button, add new gift entry.
         if giftIndex != nil {
             mainVC?.giftArray[giftIndex!] = ((giftTitle.text ?? ""), giftDescription.text, giftHyperlink.text, giftImage.image)
         }
+        // Otherwise VC was called by clicking a table section, update corresponding entry.
         else {
             mainVC?.giftArray.append((giftTitle.text ?? "", giftDescription.text, giftHyperlink.text, giftImage.image))
         }
+        // Exit VC to caller VC.
         self.navigationController?.popViewController(animated: true)
         
     }
     
+    
     @IBOutlet weak var deleteButton: UIButton!
+    
+    // Action performed when "delete" button is clicked.
     @IBAction func deleteButton(_ sender: Any) {
+        // Remove the current gift entry from giftArray.
         mainVC?.giftArray.remove(at: giftIndex!)
+        // Exit VC to caller VC.
         self.navigationController?.popViewController(animated: true)
     }
     
@@ -40,6 +60,8 @@ class NewGiftViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var giftDescription: UITextView!
     @IBOutlet weak var giftHyperlink: UITextField!
     @IBOutlet weak var giftImage: UIImageView!
+    
+    // "Select Image" button clicked, open image picker.
     @IBAction func selectImage(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
@@ -47,7 +69,7 @@ class NewGiftViewController: UIViewController, UIImagePickerControllerDelegate, 
         picker.delegate = self
         present(picker, animated: true)
     }
-    
+    // Function to set giftImage with the chosen image once image picker is used.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
         giftImage.image = image
@@ -55,10 +77,10 @@ class NewGiftViewController: UIViewController, UIImagePickerControllerDelegate, 
         dismiss(animated: true)
     }
     
+    // Assign values provided by caller VC on load.
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(mainVC)
         deleteButton.isHidden = deleteHidden
         giftTitle.text = titleVar
         giftDescription.text = descriptionVar
--- Group5FinalProject/ViewController.swift
+++ Group5FinalProject/ViewController.swift
@@ -19,27 +19,24 @@ class ViewController: UIViewController {
     // Image?
     var giftArray: [(title: String, description: String?, link: String?, image: UIImage?)] = []
     
-    let gift1 = ("Test Gift 1", "This is a test gift 1", nil as String?, UIImage(named: "marble-tile"))
-    let gift2 = ("Test Gift 2", "This is a test gift 2", "https://www.apple.com", nil as UIImage?)
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Set initial settings for tableView.
         mainTable.delegate = self
-        
         mainTable.backgroundView = UIImageView(image: UIImage(named: "marble-tile"))
         mainTable.backgroundView?.alpha = 0.5
         
     }
     override func viewWillAppear(_ animated: Bool) {
-        print(giftArray)
+        // Reload table data when this viewController reappears.
         mainTable.reloadData()
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Pass reference to this VC on segue to NewGiftViewController.
         if let vc = segue.destination as? NewGiftViewController {
             vc.mainVC = self
+            // If sender is a table cell, send information about table section and modify VC.
             if let cell = sender as? UITableViewCell {
                 vc.navigationItem.title = "Viewing Gift Entry"
                 vc.deleteHidden = false
@@ -55,22 +52,22 @@ class ViewController: UIViewController {
         }
         
     }
-
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let section = indexPath.section
-//        print("Section Clicked: \(section)")
-//    }
     
 }
 
+// Extension to work with all tableView and related functions.
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
-    
+    // Get number of sections needed to cover giftArray.
     func numberOfSections(in tableView: UITableView) -> Int {
         return giftArray.count
     }
+    
+    // Get the number of rows needed per section (four).
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
     }
+    
+    // Set custom height for image rows, dynamic height for other rows.
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 3 {
             return 200
@@ -79,18 +76,23 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
             return UITableView.automaticDimension
         }
     }
+    
+    // Set table data given data from giftArray.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         cell.textLabel?.numberOfLines = 0
+        // Ensure no data is leftover from previous calls to this func.
         cell.imageView?.image = nil
         cell.textLabel?.text = nil
         
-        //cell.imageView?.image = giftArray[indexPath.section].3
+        // Perform different actions based on current row number.
         switch indexPath.row {
+        // Assign gift title.
         case 0:
             let item = giftArray[indexPath.section].0
             cell.textLabel?.text = item
+        // Assign gift description.
         case 1:
             var text = "Description:\n"
             guard let item = giftArray[indexPath.section].1, item != "" else {
@@ -100,6 +102,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
             }
             text += item
             cell.textLabel?.text = text
+        // Assign gift hyperlink.
         case 2:
             var text = "Hyperlink:\n"
             guard let item = giftArray[indexPath.section].2, item != "" else {
@@ -109,6 +112,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
             }
             text += item
             cell.textLabel?.text = text
+        // Assign gift image.
         case 3:
             guard let image = giftArray[indexPath.section].3 else {
                 let text = "Image:\nnone"
@@ -122,11 +126,8 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         default:
             break
         }
-        
         return cell
     }
-    
-    
     
 }
 
